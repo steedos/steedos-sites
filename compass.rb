17950ba931c08334773f959b13dfddca27424d14
steedos <zhuangjianguo@hotlong.com>
2014-05-25 00:52:07 +0800

    Amazing new web site

--- compass.rb
+++ compass.rb
@@ -2,7 +2,7 @@ project_path = File.dirname(__FILE__)
 http_path    = '/'
 output_style = :compressed
 sass_dir     = 'content/css'
-css_dir      = 'output/css'
+css_dir      = 'output/website/css'
 
 sass_options = {
   :syntax => :scss
